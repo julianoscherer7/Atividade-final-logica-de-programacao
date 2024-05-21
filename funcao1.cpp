1c2b2063b06ebd051976b9f4ac2b55abf0409f99
julianoscherer7 <162385579+julianoscherer7@users.noreply.github.com>
2024-05-21 00:40:58 +0000

    a

--- funcao1.cpp
+++ funcao1.cpp
@@ -8,18 +8,22 @@ string Titulo[1000];
 string Descricao[1000];
 string DataDeVencimento[1000];
 string status[1000];
+int controle = 0;
 int main () {
-    cout << "Olá, seja bem vindo ao Gerenciador de Tarefas. Escolha uma função: " << endl;
+
+    do
+    {
+        cout << "Olá, seja bem vindo ao Gerenciador de Tarefas. Escolha uma função: " << endl;
     cout << "1. Adicione uma tarefa "<< endl;
     cout << "2. Visualize suas tarefas" << endl; 
     cout << "3. Remova uma tarefa "<< endl; 
     cout << "4. Edite uma tarefa" << endl;
     cout << "5. Busque uma tarefa" << endl;
     cout << "6. Filtrar tarefas" << endl;
-    cout << "0. Sair" << endl; 
+    cout << "0. Sair" << endl;
     cin >> menu;
-
-    if (menu == 1) {
+     
+     if (menu == 1) {
         cout << "ID: ";
         cin >> ID[1000];
         cout << "Titulo: ";
@@ -30,8 +34,28 @@ int main () {
         cin.ignore(); getline(cin, DataDeVencimento[1000]);
         cout << "Status: " << endl;
         cin.ignore(); getline(cin, status[1000]);
-
+    controle ++;
 
     }
-    return 0;
-}   
+    
+  
+
+    if  (menu == 2) {
+        for (int i = 0; i < controle; i++)
+        {
+          
+        
+         
+        cout << ID[i] << endl;
+        cout << Titulo[i] << endl;
+        cout << Descricao[i] << endl;
+        cout << DataDeVencimento[i] << endl;
+        cout << status[i] << endl;
+    }   }
+    
+    } while (menu != 0);
+    
+    
+
+        return 0;    
+    }
